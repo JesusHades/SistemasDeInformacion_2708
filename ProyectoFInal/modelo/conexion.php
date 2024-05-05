8109ad2ac0162c5729b8f89bcb0da78e8a2fa98d
“JesusHades” <jesusmanriquez56@aragon.unam.mx>
2024-05-05 15:41:19 -0600

    Se agregan datos para la conxion

---
+++ ProyectoFInal/modelo/conexion.php
@@ -0,0 +1,5 @@
+<?php
+
+$conexion = new mysqli("localhost", "root", "123456", "ProyectoFinalSI", "3306"); //datos para la conexion con DB
+$conexion -> set_charset("utf8"); //Para que se reconozcan caracteres especiales
+?>
