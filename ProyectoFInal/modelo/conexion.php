fcc6ca3d02595c0d835e857f3d7d68a86e3a37d9
“JesusHades” <jesusmanriquez56@aragon.unam.mx>
2024-05-05 16:31:19 -0600

    Se cambio el nombre de la db

--- ProyectoFInal/modelo/conexion.php
+++ ProyectoFInal/modelo/conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-$conexion = new mysqli("localhost", "root", "123456", "ProyectoFinalSI", "3306"); //datos para la conexion con DB
+$conexion = new mysqli("localhost", "root", "123456", "proyectofinalsi", "3306"); //datos para la conexion con DB
 $conexion -> set_charset("utf8"); //Para que se reconozcan caracteres especiales
 ?>
