8fe4f097534f76a5f963715a5c06e7be85b94fe2
“JesusHades” <jesusmanriquez56@aragon.unam.mx>
2024-05-05 19:23:11 -0600

    Se agrega cnd de bootstrap para los estilos

--- ProyectoFInal/editar.php
+++ ProyectoFInal/editar.php
@@ -7,10 +7,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Editar Registros</title>
 </head>
 <body>
-<form class="col-4 p-3" method="POST">
+<form class="col-4 p-3 m-auto" method="POST">
         <h3 class="text-center text-secondary">Modificar Registro de Alumno</h3>
 
         <?php
