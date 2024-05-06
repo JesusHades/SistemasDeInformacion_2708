747f8f67d086c5ed6bea75457d38c66ee7413096
“JesusHades” <jesusmanriquez56@aragon.unam.mx>
2024-05-05 20:49:05 -0600

    Se agrega logica para eliminar registros

---
+++ ProyectoFInal/controlador/eliminarAlumno.php
@@ -0,0 +1,17 @@
+<?php
+
+if(!empty($_GET["id"])) {
+
+    $id = $_GET["id"];
+    $sql = $conexion->query(" delete from alumnos where id=$id ");
+
+    if ($sql==1) {
+        echo '<div class="alert alert-success">Alumno Eliminado Correctamente</div>';
+    } else {
+        echo '<div class="alert alert-danger">Error al Eliminar Alumno</div>';
+    }
+    
+
+}
+
+?>
